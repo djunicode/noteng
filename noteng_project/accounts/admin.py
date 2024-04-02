31471281880669933a6b031c1260451a9932a80e
Kshitij04Poojary <kpoojary2004@gmail.com>
2024-04-02 19:56:24 +0530

    videolinks, postmodel, jobboardmodel and event model made by Mehek Mehta is commited
    committed Videolinks, Post, Job Board,event models

--- noteng_project/accounts/admin.py
+++ noteng_project/accounts/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import User,Professor,Student
+from .models import User,Professor,Student,videolinksModel,postModel,jobBoardModel,eventModel
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Professor)
 admin.site.register(Student)
+admin.site.register(videolinksModel)
+admin.site.register(postModel)
+admin.site.register(jobBoardModel)
+admin.site.register(eventModel)
--- noteng_project/accounts/models.py
+++ noteng_project/accounts/models.py
@@ -55,3 +55,56 @@ class Student(User):
     
     current_year = models.CharField(max_length=20, choices=YEAR_CHOICES)
     branch = models.CharField(max_length=100)
+
+class videolinksModel(models.Model):
+    sapid=models.ForeignKey(User,on_delete=models.CASCADE)
+    subject=models.CharField(max_length=100)
+    sem=models.IntegerField()
+    topics=models.CharField(max_length=100)
+    links=models.URLField()
+    video_id=models.AutoField(primary_key=True)
+
+class postModel(models.Model):
+    postType= [
+          ('Job','job_board'),
+          ('Event','event')]
+    sapid=models.ForeignKey(User,on_delete=models.CASCADE)
+    post_id=models.AutoField(primary_key=True)
+    title=models.TextField()
+    deadline=models.DateTimeField()
+    post_url=models.URLField()
+    description=models.TextField()
+    likes=models.PositiveIntegerField()
+    type=models.CharField(max_length=10,choices= postType)
+    is_interested=models.BooleanField(default=False)
+    date_updated=models.DateField(auto_now=True)
+    date_uploaded=models.DateField(auto_now=True)
+
+class jobBoardModel(postModel):
+    jobsubtype=[
+        ('internship','internship'),
+        ('Job','job')
+    ]
+    status=[
+        ('Offline','offline'),
+        ('Online','on;ine')
+    ]
+    job_id=models.AutoField(primary_key=True)
+    company=models.CharField(max_length=50)
+    subtype=models.CharField(max_length=10,choices=jobsubtype)
+    mode=models.CharField(max_length=10,choices=status)
+    
+class eventModel(postModel):
+    organizer=[
+        ('college','college'),
+        ('commitee','commitee')
+    ]
+    eventType=[
+        ('hackathon','hackathon'),
+        ('cultural','cultural'),
+        ('datathon','datathon'),
+        ('startup','startup')
+    ]
+    event_id=models.AutoField(primary_key=True)
+    organised_by=models.CharField(max_length=50,choices=organizer)
+    subtype=models.CharField(max_length=20,choices=eventType)
