1acc45984dd615136dff8f0cc945f12bc3cfed3c
Divya Gandhi <divyagandhi274@gmail.com>
2024-04-04 11:07:13 +0530

    Sidebar changes

--- src/Components/Home/Sidebarresponsive.jsx
+++ src/Components/Home/Sidebarresponsive.jsx
@@ -4,12 +4,13 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 function Sidebarresponsive() {
     
   return (
- 
-        <div className='flex bg-custom-blue p-5'>
+    <div className='flex w-full'>
+        <div className='flex bg-custom-blue p-5 w-full'>
             <div className='h-8 w-8 bg-white'>
                 <MoreVertIcon/>
             </div>
            
+        </div>
         </div>
    
   )
