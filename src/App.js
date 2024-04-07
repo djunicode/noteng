a355dc9da8780f80138e43e012d4e047917ac15d
Divya Gandhi <divyagandhi274@gmail.com>
2024-04-07 16:40:26 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
 import './App.css'
 import Home from './Pages/Home'
-import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import PostJob from './Pages/PostJob';
 import ViewJob from './Pages/ViewJob';
-<<<<<<< HEAD
 import NewPost from './Pages/NewPostPage';
 import UploadNewPosts from './Pages/UploadNewPosts';
 
-=======
->>>>>>> 290f7b5b6760637f9cb9858b2b669fa12e27c5a4
 function App() {
   return (
-   <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
-        
-            <Route path='/' element={<Home />} />
-            <Route path='/NewPost' element={<NewPost/>}/>
-            <Route path='/createjob' element={<PostJob />} />
-            <Route path='/viewjob' element={<ViewJob />} />        
-           <Route path='/UploadNewPosts' element={<UploadNewPosts/>}/>
-=======
-        <Route path='/' element={<Home/>}/>
-        <Route path='/createjob' element={<PostJob/>}/>
-        <Route path='/viewjob' element={<ViewJob/>}/>
->>>>>>> 290f7b5b6760637f9cb9858b2b669fa12e27c5a4
+        <Route path='/' element={<Home />} />
+        <Route path='/NewPost' element={<NewPost />} />
+        <Route path='/createjob' element={<PostJob />} />
+        <Route path='/viewjob' element={<ViewJob />} />
+        <Route path='/UploadNewPosts' element={<UploadNewPosts />} />
       </Routes>
-   </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
-=======
 import "@fontsource/poppins"; // Defaults to weight 400
->>>>>>> 290f7b5b6760637f9cb9858b2b669fa12e27c5a4
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,4 +16,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
