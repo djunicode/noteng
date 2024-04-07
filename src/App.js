290f7b5b6760637f9cb9858b2b669fa12e27c5a4
MonilMehta <monilmehta5@gmail.com>
2024-04-07 15:01:24 +0530

    Revert "ansh shah@DESKTOP-5GQJ8J9 MINGW64 /d/React/React-Login-App (frontend)"
    This reverts commit b07422c8966e20c0dd6adf2426b5a16008c7758a.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import './App.css'
 import Home from './Pages/Home'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import PostJob from './Pages/PostJob';
 import ViewJob from './Pages/ViewJob';
-
-import Register from './LoginPage/Register';
-import Login from './LoginPage/Login';
-import Layout from './LoginPage/Layout';
-import Unauthorized from './LoginPage/Unauthorized';
-import LinkPage from './LoginPage/LinkPage';
-import RequireAuth from './LoginPage/RequireAuth';
-
-const ROLES = {
-  'User': 2001
-}
-
 function App() {
   return (
-    <BrowserRouter>
+   <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* public routes */}
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="linkpage" element={<LinkPage />} />
-          <Route path="unauthorized" element={<Unauthorized />} />
-
-          {/* we want to protect these routes */}
-          <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-            <Route path='/' element={<Home />} />
-            <Route path='/createjob' element={<PostJob />} />
-            <Route path='/viewjob' element={<ViewJob />} />        
-          </Route>
-
-        </Route>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/createjob' element={<PostJob/>}/>
+        <Route path='/viewjob' element={<ViewJob/>}/>
       </Routes>
-    </BrowserRouter>
+   </BrowserRouter>
   );
 }
 
