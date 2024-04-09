c2c50cb93792cb629037be6e13262a566e04d599
anshshah23 <anshzeal2328@gmail.com>
2024-04-09 16:08:59 +0530

    Login and Signup pages added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css'
 import Home from './Pages/Home'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './Pages/Login';
+import SignUp from './Pages/Signup';
 import PostJob from './Pages/PostJob';
 import ViewJob from './Pages/ViewJob';
 import NewPost from './Pages/NewPostPage';
@@ -9,6 +12,28 @@ import UploadNewPosts from './Pages/UploadNewPosts';
 function App() {
   return (
     <BrowserRouter>
+    <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={'/sign-in'}>Noteng</Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item"><Link className="nav-link" to={'/sign-in'}>Login</Link></li>
+                <li className="nav-item"><Link className="nav-link" to={'/sign-up'}>Sign up</Link></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/Login" element={<Login />} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/NewPost' element={<NewPost />} />
