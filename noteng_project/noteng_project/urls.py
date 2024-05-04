26fc6264c246c344c1e7e79617352e997e4d042d
MonilMehta <monilmehta5@gmail.com>
2024-05-04 20:22:30 +0530

    Changed urls.py name from accounts to api😂

--- noteng_project/noteng_project/urls.py
+++ noteng_project/noteng_project/urls.py
@@ -37,7 +37,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include('authentication.urls')), 
-    path("accounts/", include('accounts.urls')), 
+    path("api/", include('accounts.urls')), 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
