c1408bba881846365cf6ac6285f8d18d5e215c29
mehekmehta06 <133655764+mehekmehta06@users.noreply.github.com>
2024-05-05 15:54:01 +0530

    changed videolinks serializer
    added code to convert yt links to embedded yt link when get request is made.

--- noteng_project/accounts/serializers.py
+++ noteng_project/accounts/serializers.py
@@ -3,6 +3,7 @@ from .models import CalendarModel, PostModel,NotesModel,JobBoardModel,Videolinks
 from authentication.models import User
 from authentication.serializers import UserSerializer
 from django.core.exceptions import ValidationError
+import re
 class CalendarSerializer(serializers.ModelSerializer):
     class Meta:
         model = CalendarModel
@@ -34,8 +35,31 @@ class JobBoardSerializer(serializers.ModelSerializer):
         
 class VideolinksSerializer(serializers.ModelSerializer):
     class Meta:
-        model= VideolinksModel
+        model = VideolinksModel
         fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        youtube_link = data.get('links', '')
+        embedded_link = self.convert_to_embed_link(youtube_link)
+        if embedded_link:
+            data['links'] = embedded_link
+        return data
+    
+    @staticmethod
+    def convert_to_embed_link(youtube_link):
+    # Regular expression pattern to extract video ID
+        pattern = r'(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})'
+    # Search for video ID in the YouTube link
+        match = re.search(pattern, youtube_link)
+        if match:
+            video_id = match.group(1)
+        # Construct embedded link
+            embedded_link = f"https://www.youtube.com/embed/{video_id}"
+            return embedded_link
+        else:
+            return None
+    
     
 # class EventSerializer(serializers.ModelSerializer):
 #     class Meta:
