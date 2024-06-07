c43353658ab74e287b901d65e1be4e043b9bdaaa
Divya Gandhi <divyagandhi274@gmail.com>
2024-06-07 17:48:34 +0530

    Changes in Discover Page

--- src/Pages/DiscoverPage.jsx
+++ src/Pages/DiscoverPage.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Sidebar from '../Components/Home/Sidebar'
 import Discover from '../Components/Discover/Discover'
-
+import Cards from '../Components/Discover/Cards'
+import CardsCoulmn from '../Components/Discover/CardsCoulmn'
 function DiscoverPage() {
   return (
     <div className='flex flex-col lg:flex-row'>
         <Sidebar/>
-        <div className='flex '>
+        <div className='flex flex-col '>
             <Discover/>
+            <Cards/>
+            <CardsCoulmn/>
         </div>
 
     </div>
